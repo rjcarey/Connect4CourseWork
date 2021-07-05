1e3da252e878735527d8a7ac1526d61a9bc4f94a
Ruaidhri Carey <rjcarey@perse.co.uk>
2021-07-05 14:32:18 +0000

    Objective 1 complete and tested

--- ConnectFour.py
+++ ConnectFour.py
@@ -2,10 +2,19 @@ from sys import argv
 
 def usage():   
     print(f"""
-Usage: {argv[0]} [g | t]
+Error: Incorrect run command
+Command: ConnectFour.py [g | t]
 g : play with the GUI
 t : play with the Terminal""")
     quit()
 
 if __name__ == "__main__":
-    pass
+    if len(argv) == 2:
+        if argv[1] == "t":
+            print("initialising terminal mode...")
+        elif argv[1] == "g":
+            print("initialising gui mode...")
+        else:
+            usage()
+    else:
+        usage()
