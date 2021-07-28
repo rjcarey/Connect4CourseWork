ffe340e454a35db82b21f352b8a47854f9f12473
Ruaidhri <carey.ruaidhri@gmail.com>
2021-07-28 10:50:11 +0100

    Easy ai program fixed, it now plays in a random row of the rows with the highest score.

--- players.py
+++ players.py
@@ -28,17 +28,24 @@ class Ai:
     def easyAI(self, board, counter):
         scores = [0,0,0,0,0,0,0]
         for column in range(7):
+            #print(f"column {column + 1}:")
             # check for full row
             if board[0][column] != " ":
+                #print(f"{column} full")
                 scores[column] = -1
                 break
 
             #find the row that would be played in
+            #print(board)
             for i, row in enumerate(board):
+                #print(f"{i}, {row}")
                 if row[column] != " ":
                     playedRow = i - 1
+                    break
                 elif i == 5:
                     playedRow = 5
+
+            #print(f"played row: {playedRow}")
 
             #check what counters this play would connect with and calculate resulting score
             leftOne = True if column > 0 else False
@@ -53,98 +60,118 @@ class Ai:
             upOne = True if playedRow > 0 else False
             upTwo = True if playedRow > 1 else False
             upThree = True if playedRow > 2 else False
-            print(f"{column}, {playedRow}: {leftOne}, {leftTwo}, {leftThree}, {downOne}, {downTwo}, {downThree}, {rightOne}, {rightTwo}, {rightThree}, {upOne}, {upTwo}, {upThree}")
+            #print(f"{column}, {playedRow}: {leftOne}, {leftTwo}, {leftThree}, {downOne}, {downTwo}, {downThree}, {rightOne}, {rightTwo}, {rightThree}, {upOne}, {upTwo}, {upThree}")
 
             score = 0
             currentScore = 0
             if upOne and leftOne:
                 if board[playedRow - 1][column - 1] == counter:
+                    #print("upOne and leftOne")
                     currentScore += 1
                     if upTwo and leftTwo:
                         if board[playedRow - 2][column - 2] == counter:
+                            #print("upTwo and leftTwo")
                             currentScore *= 2
                             if upThree and leftThree:
                                 if board[playedRow - 3][column - 3] == counter:
+                                    #print("upThree and leftThree")
                                     currentScore *= 9999
 
             score += currentScore
             currentScore = 0
             if leftOne:
                 if board[playedRow][column - 1] == counter:
+                    #print("leftOne")
                     currentScore += 1
                     if leftTwo:
                         if board[playedRow][column - 2] == counter:
+                            #print("leftTwo")
                             currentScore *= 2
                             if leftThree:
                                 if board[playedRow][column - 3] == counter:
+                                    #print("leftThree")
                                     currentScore *= 9999
 
             score += currentScore
             currentScore = 0
-
             if downOne and leftOne:
                 if board[playedRow + 1][column - 1] == counter:
+                    #print("downOne and leftOne")
                     currentScore += 1
                     if downTwo and leftTwo:
                         if board[playedRow + 2][column - 2] == counter:
+                            #print("downTwo and leftTwo")
                             currentScore *= 2
                             if downThree and leftThree:
                                 if board[playedRow + 3][column - 3] == counter:
+                                    #print("downThree and leftThree")
                                     currentScore *= 9999
 
             score += currentScore
             currentScore = 0
             if downOne:
                 if board[playedRow + 1][column] == counter:
+                    #print("downOne")
                     currentScore += 1
                     if downTwo:
                         if board[playedRow + 2][column] == counter:
+                            #print("downTwo")
                             currentScore *= 2
                             if downThree:
                                 if board[playedRow + 3][column] == counter:
+                                    #print("downThree")
                                     currentScore *= 9999
 
             score += currentScore
             currentScore = 0
             if downOne and rightOne:
                 if board[playedRow + 1][column + 1] == counter:
+                    #print("downOne and rightOne")
                     currentScore += 1
                     if downTwo and rightTwo:
                         if board[playedRow + 2][column + 2] == counter:
+                            #print("downTwo and rightTwo")
                             currentScore *= 2
                             if downThree and rightThree:
                                 if board[playedRow + 3][column + 3] == counter:
+                                    #print("downThree and rightThree")
                                     currentScore *= 9999
 
             score += currentScore
             currentScore = 0
             if rightOne:
                 if board[playedRow][column + 1] == counter:
+                    #print("rightOne")
                     currentScore += 1
                     if rightTwo:
                         if board[playedRow][column + 2] == counter:
+                            #print("rightTwo")
                             currentScore *= 2
                             if rightThree:
                                 if board[playedRow][column + 3] == counter:
+                                    #print(rightThree)
                                     currentScore *= 9999
 
             score += currentScore
             currentScore = 0
             if upOne and rightOne:
                 if board[playedRow - 1][column + 1] == counter:
+                    #print("upOne and rightOne")
                     currentScore += 1
                     if upTwo and rightTwo:
                         if board[playedRow - 2][column + 2] == counter:
+                            #print("upTwo and rightTwo")
                             currentScore *= 2
                             if upThree and rightThree:
                                 if board[playedRow - 3][column + 3] == counter:
+                                    #print("upThree and rightThree")
                                     currentScore *= 9999
 
             score += currentScore
             scores[column] = score
 
         #find the best scoring move
-        print(scores)
+        #print(scores)
         maxScore = -1
         maxIndex = []
         for i, score in enumerate(scores):
